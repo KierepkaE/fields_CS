b46bc28b77db375b95d8cf13c61553c81108d4cc
kierepkae <kierepkaedyta@gmail.com>
2019-11-29 09:48:38 +0000

    create Customer and Order classes

---
+++ Customer.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Fields {
+  class Customer {
+    public int Id;
+    public string Name;
+  };
+}
---
+++ Order.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace Fields {
+  class Order {
+
+  };
+}
