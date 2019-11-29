c25f811484c8f32348d81303d9da6ac3400f5810
kierepkae <kierepkaedyta@gmail.com>
2019-11-29 16:38:37 +0000

    refactor2

--- Person.cs
+++ Person.cs
@@ -2,11 +2,7 @@ using System;
 
 namespace Fields {
   class Person {
-    private DateTime _birthdate;
-    public DateTime Birthdate {
-      get { return _birthdate };
-      set { _birthdate = value };
-    }
+    private DateTime Birthdate { get; set; }
 
   };
 }
