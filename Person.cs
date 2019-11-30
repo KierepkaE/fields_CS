8f9f3ac28bae36703edc406045fe59854f529969
kierepkae <kierepkaedyta@gmail.com>
2019-11-30 11:26:16 +0000

    add Age field to Person

--- Person.cs
+++ Person.cs
@@ -4,5 +4,13 @@ namespace Fields {
   class Person {
     private DateTime Birthdate { get; set; }
 
+    public int Age {
+      {
+        get { var timespanDateTime.Today - Birthdate };
+        var years = timeSapn.Days / 365;
+        return years;
+      }
+    };
+
   };
 }
